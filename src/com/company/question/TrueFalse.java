6e4f914f51cd44d5722d20f2d29cc9566262a3a0
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2020-07-18 22:21:02 -0400

    Added required classes

---
+++ src/com/company/question/TrueFalse.java
@@ -0,0 +1,23 @@
+package com.company.question;
+
+import java.util.ArrayList;
+
+public class TrueFalse extends Base{
+    public TrueFalse(String question, String correctAnswer) {
+        super(question, correctAnswer);
+    }
+
+    @Override
+    public String toString() {
+        return  "\nPlease answer YES(y) or NO(n): " +
+                getQuestion();
+    }
+
+    public Boolean checkingAnswer() throws Exception {
+        System.out.println(getUserAnswer().toLowerCase());
+        if (!"yn".contains(getUserAnswer().toLowerCase())){
+            throw new Exception("Please try again. Only (y/n) allowed");
+        }
+        return getCorrectAnswer().equals(getUserAnswer());
+    }
+}
